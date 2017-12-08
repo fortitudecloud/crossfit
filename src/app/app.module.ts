617854ab331681a877cdeccd2f228a8cced0928c
Hickey, Lionel <Lionel.Hickey@sew.com.au>
2017-12-08 11:50:10 +1100

    added map function to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
+// Routing
+import { routing } from './app.route';
 
+// Components
 import { AppComponent } from './app.component';
+
+// Views
+import { MapBuilderViewComponent } from './views/admin/mapbuilder/mapbuilder.view';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MapBuilderViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, FormsModule, routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBCVJPfwKSgIrz0VWZDL6LNUWMK7M6oR_w'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.route.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//import { ViewRoute } from './views/view.route';
+
+import { MapBuilderViewComponent } from './views/admin/mapbuilder/mapbuilder.view';;
+
+const appRoutes: Routes = [    
+    {path: '', component: MapBuilderViewComponent}    
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
