859f264e5a94670dc56276dc8063e954fe440a9a
Hickey, Lionel <Lionel.Hickey@sew.com.au>
2017-12-14 15:59:13 +1100

    if time

---
+++ src/app/interface/poker.interface.ts
@@ -0,0 +1,9 @@
+
+// https://desandro.github.io/3dtransforms/examples/card-01.html
+
+/** poker cards used in achievements */
+export class IPokerCard {
+    id: string;
+    number: number;
+    suit: string;
+}
