80f8bfe6e73455f70074a96badfd991ecf899f78
Lionel Hickey <lionel.hickey@outlook.com>
2017-12-15 07:13:15 +1100

    added mapviewer

---
+++ src/app/component/map/mapviewer/mapviewer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+
+@Component({
+    selector: 'map-viewer',
+    styleUrls: ['./mapviewer.component.scss'],
+    templateUrl: './mapviewer.component.html'
+})
+export class MapViewerComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {}
+
+}
